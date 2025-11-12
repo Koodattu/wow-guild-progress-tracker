d37673e3d9bc0d1fd1896a8fd4b896a3bdf88837
Juha Ala-Rantala <alarantala.juha@gmail.com>
2025-11-12 20:31:09 +0200

    Update guilds.ts

--- backend/src/config/guilds.ts
+++ backend/src/config/guilds.ts
@@ -36,7 +36,7 @@ export const GUILDS_PROD: TrackedGuild[] = [
   { name: "Forbidden", realm: "Vashj", region: "EU" },
   { name: "Tony Halme Pro Skater", realm: "Stormreaver", region: "EU" },
   { name: "Slack", realm: "Stormreaver", region: "EU" },
-  { name: "Kelacity", realm: "Stormreaver", region: "EU" },
+  { name: "Kelacity", realm: "Stormreaver", region: "EU", parent_guild: "Tony Halme Pro Skater" },
   { name: "Muisted", realm: "Stormreaver", region: "EU" },
   { name: "TURBO SAAB", realm: "Stormreaver", region: "EU" },
   { name: "Rennosti", realm: "Stormreaver", region: "EU" },
